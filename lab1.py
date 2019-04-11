8cd04472d17549dcfc1234f8e869175e0862fd68
stmelgoz <49253723+stmelgoz@users.noreply.github.com>
2019-04-10 22:45:03 -0700

    Implemented files

--- lab1.py
+++ lab1.py
@@ -2,14 +2,44 @@
 def max_list_iter(int_list):  # must use iteration not recursion
    """finds the max of a list of numbers and returns the value (not the index)
    If int_list is empty, returns None. If list is None, raises ValueError"""
-   pass
+   
+   max = 0
+   if int_list == []:
+       return None
+   if int_list == None:
+       raise ValueError('no list')
+   for i in range(len(int_list)):
+       if int_list[i] > max:
+           max = int_list[i]
+   return max
 
 def reverse_rec(int_list):   # must use recursion
    """recursively reverses a list of numbers and returns the reversed list
-   If list is None, raises ValueError"""
-   pass
-
+   If list is None, raises ValueError""
+   
+   if int_list == None:
+       raise ValueError
+   elif (len(int_list) == 1):
+       return int_list[0]
+   else:
+       newl = int_list[-(len(int_list)-1):-1]
+       new_intl = reverse_rec(newl)
+       return new_intl
+   
 def bin_search(target, low, high, int_list):  # must use recursion
    """searches for target in int_list[low..high] and returns index if found
-   If target is not found returns None. If list is None, raises ValueError """
-   pass
+   If target is not found returns None. If list is None, raises ValueError"""
+   if int_list == None:
+       raise ValueError
+   else:
+       if high > low: 
+           mid = 1 + (len(int_list)-1)/2
+           if int_list[mid] == target:
+               return mid
+           elif int_list[mid] > target:
+               return bin_search(target, low, mid-1, int_list)
+           elif int_list[mid] < target:
+                return bin_search(target, mid+1, high, int_list)
+           else:
+                return None
+   
--- location_tests.py
+++ location_tests.py
@@ -6,8 +6,25 @@ class TestLab1(unittest.TestCase):
     def test_repr(self):
         loc = Location("SLO", 35.3, -120.7)
         self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
+        loc2 = Location("New York", 123.5, 234.4)
+        self.assertEqual(repr(loc2),"Location('New York', 123.5, 234.4)")
+        loc3 = Location("Oxnard", 143.5, -87.8)
+        self.assertEqual(repr(loc3),"Location('Oxnard', 143.5, -87.8)")
     
     # Add more tests!
+    def test_eq(self):
+        loc = Location("SLO", 35.3, -120.7)
+        loc2 = Location("SLO", 35.3, -120.7)
+        self.assertEqual(loc.__eq__(loc2), loc == loc2)
+        loc3 = Location("New York", 123.5, 234.4)
+        loc4 = Location("New York", 123.5, 234.4)
+        self.assertEqual(loc3.__eq__(loc4), loc3 == loc4)
+        loc5 = Location("Oxnard", 143.5, -87.8)
+        loc6 = Location("Oxnard", 143.5, -87.8)
+        self.assertEqual(loc5.__eq__(loc6), loc5 == loc6)
+        loc7 = Location("Ventura", 167.5, -40.3)
+        loc8 = Location("Ventura", 167.5, -40.3)
+        self.assertEqual(loc7.__eq__(loc8), loc7 == loc8)
 
 if __name__ == "__main__":
         unittest.main()
